41dd412a8ca76fb0e7acb4762bf621d81a5285e9
murata-naoya <murata.small.baseball@gmail.com>
2023-01-14 10:23:59 +0900

    [feat] storybookのインストール

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import { StorybookConfig } from "@storybook/vue3-vite";
+
+const config: StorybookConfig = {
+  framework: "@storybook/vue3-vite",
+  stories: ["../src/**/*.stories.@(js|ts)"],
+};
+
+export default config;
+
+// const path = require('path');
+// module.exports = {
+//   "stories": [
+//     "../src/**/*.mdx",
+//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
+//   ],
+//   "addons": [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-interactions"
+//   ],
+//   "framework": {
+//     "name": "@storybook/vue3-vite",
+//     "options": {}
+//   },
+//   "docs": {
+//     "autodocs": "tag"
+//   }
+// }
